b7993ae821a362d493bcc93be43031d997936a90
Legendbhai1 <lrheflahola@gmail.com>
2026-08-06 13:12:51 +0000

    Remove all existing files

--- bot/utils/id.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-/** Generates a deal ID like ESC-8F42A1 */
-function generateDealId() {
-  const suffix = crypto.randomBytes(3).toString('hex').toUpperCase();
-  return `ESC-${suffix}`;
-}
-
-module.exports = { generateDealId };
